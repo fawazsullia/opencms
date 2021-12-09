6cba4be6d3c63733ba47352115bbafadc2b400a5
Fawaz Sullia <fawazsullia@gmail.com>
2021-12-09 13:59:25 +0400

    login functionality in profile routes, login error handling

--- routes/profileRoutes/authentication.js
+++ routes/profileRoutes/authentication.js
@@ -1,9 +1,12 @@
 //register, login
+// what if they enter wrong details?
+//
 const express = require('express')
 const router = express.Router()
 const UserModel = require('../../models/userModel')
 
-// *register a new account
+//* register a new account
+
 router.post('/register', async (req, res)=> {
 
 const { userName, userId, userEmail } = req.body
@@ -26,6 +29,32 @@ catch(e){
 res.status(500).json({message : "Something went wrong"}).end()
 }
 
+
+})
+
+
+//* login to an existing account
+
+router.post('/login', async (req, res)=> {
+
+const { userId } =  req.body
+
+try{
+
+const data = await UserModel.findOne({ userId : userId })
+
+if(data){
+    await res.status(200).json(data).end()
+}
+else{
+    await res.status(400).json({message : "No such user found"}).end()
+}
+
+}
+catch(e){
+    console.log(e)
+    res.status(500).json({message : "Something went wrong"}).end()
+ }
 
 })
 
