24d8f9bc6fb274cc3394d002b558b3b2f4dc4f58
Fawaz Sullia <fawazsullia@gmail.com>
2021-12-09 14:50:53 +0400

    add edit profile route

--- routes/profileRoutes/editProfile.js
+++ routes/profileRoutes/editProfile.js
@@ -1,1 +1,26 @@
 //change name, email etc
+
+const express = require('express')
+const router = express.Router()
+const UserModel = require('../../models/userModel')
+
+//* edit profile
+
+router.put('/editprofile', async (req, res)=> {
+
+const data = req.body
+
+try{
+await UserModel.findOneAndUpdate({ userId : data.userId }, data)
+await res.status(200).json({message : "Updated successfully"}).end()
+}
+catch(e){
+res.status(500).json({message : "Something went wrong"}).end()
+}
+
+})
+
+
+
+
+module.exports = router
