c8d3578e1b759f27d23cf4a2e13f10af459c4cbf
Fawaz Sullia <fawazsullia@gmail.com>
2021-12-14 12:34:05 +0400

    added route to fetch content when accessed via a link

--- routes/contentRoutes/textRoutes.js
+++ routes/contentRoutes/textRoutes.js
@@ -18,7 +18,7 @@ const { projectId, content, title, description, userName  } = req.body
 //create the schema for content in the project
 
 const id = new mongoose.Types.ObjectId()
-const url = `http://localhost:5000/text/${projectId}/${id}`
+const url = `http://localhost:5000/text/${id}`
 
 const reqContent = {
     contentId : id,
@@ -32,12 +32,46 @@ const reqContent = {
 
 //update the project to add the text content
 try{
-await ProjectModel.findOneAndUpdate( { projectId : projectId}, { $push : { projectContent : [reqContent]  } })
+const project = await ProjectModel.findOne({projectId : projectId})
+if(project){
+
+await ProjectModel.findOneAndUpdate( { projectId : projectId}, { projectContent : {[id] : reqContent}   })
 await res.status(200).json({ message : "Successfully created"}).end()
+
+}
+else { res.status(400).json({message : "Project not found"}).json()  }
+
 }
 catch(e){
     console.log(e)
     res.status(500).json({message : "Something went wrong"}).end()
+}
+
+})
+
+//* access the text content 
+//this is queried and only gives back th eactual content
+
+router.get('/:projectId/:contentId', async (req, res)=>{
+
+const { projectId , contentId } = req.params
+try{
+const project = await ProjectModel.findOne({ projectId : projectId } , `projectContent`   )
+
+
+
+
+
+
+if(project){
+    const proContent = project.projectContent
+const content = proContent[contentId].content
+    await res.status(200).json(content).end()
+}
+else {await res.status(400).json({message : "Project Not found"}).end() }
+}
+catch(e){
+res.status(500).json({message : "something went wrong"}).end()
 }
 
 })
